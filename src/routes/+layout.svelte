a2ec73e7b310fe31ab01d3a7243f54a3441a3214
Aparimeya Taneja <at307@duke.edu>
2022-12-18 18:18:17 -0800

    banner under construction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,30 @@
 <script>
-	import Header from './Header.svelte';
+	// import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
+	<!-- <Header /> -->
 
-	<main>
+	
 		<slot />
-	</main>
 
-	<footer>
+
+	<!-- <footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</footer> -->
 </div>
 
 <style>
 	.app {
 		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+
+		background-color: rgb(10, 10, 10);
+		position: relative;
+		align-items: center;
+height: 100%;
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 
 	footer {
 		display: flex;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,18 @@
 <script>
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>aparieya.ai</title>
+	<title>aparimeya.ai</title>
 	<meta name="description" content="Ap" />
 </svelte:head>
 
 <section>
-	hi
+	<h1>
+		Under Construction 
+	</h1>
+	🤫
 </section>
 
 <style>
@@ -19,7 +21,40 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
+		height: 350px;
+		width: 300px;
+		background-color: rgba(20, 20, 20,.2);
+		color: white;
+		border-radius: 10px;
+		box-shadow:
+    inset 0 0 5px  rgba(255, 255, 255,.95),
+	inset 20px 0 80px rgba(255, 0, 255,.5),
+    inset -20px 0 80px rgba(0, 255, 255,.5),
+    inset 20px 0 300px rgba(255, 0, 255,.5),
+    inset -20px 0 300px rgba(0, 255, 255,.5),
+    0 0 50px rgba(255, 255, 255,.5),
+    -10px 0 80px rgba(255, 0, 255,.5),
+    10px 0 80px rgba(0, 255, 255,.5);
+
 	}
+
+	h1 {
+
+		background-color: red;
+  
+  /* Create the gradient. */
+  background-image: linear-gradient(45deg, #8fc3a4, #bf91ff);
+  
+  /* Set the background size and repeat properties. */
+  background-size: 100%;
+  background-repeat: repeat;
+
+  /* Use the text as a mask for the background. */
+  /* This will show the gradient as a text color rather than element bg. */
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent; 
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+}
 	
 </style>
