a97d052d0e581429e47cef30a558f677230df54e
Aparimeya Taneja <42758991+humblef00ls@users.noreply.github.com>
2022-12-27 01:51:59 +0000

    removed q

--- src/lib/Simulation.svelte
+++ src/lib/Simulation.svelte
@@ -1,13 +1,11 @@
 <script>
     let canvas;
     import { onMount } from "svelte";
-    import { QuadTree, Rectangle, Point, Circle } from "$lib/QuadTree";
     export let y = 0;
     let m = { x: 0, y: 0 };
-    const local = 100;
-    const damp = 0.95;
+    let local = 150;
+    const damp = 0.85;
     const margin = 5;
-    const cap = 15
     function handleMousemove(event) {
         m.x = event.clientX;
         m.y = event.clientY;
@@ -17,15 +15,13 @@
         let h = window.innerHeight;
         let w = window.innerWidth;
 
-
+        console.log(w, h);
         canvas.height = h;
         canvas.width = w;
 
         const ctx = canvas.getContext("2d", { alpha: false });
         const dpr = window.devicePixelRatio;
         const rect = canvas.getBoundingClientRect();
-        let boundary = new Rectangle(0, 0, w, h);
-        let qtree = new QuadTree(boundary, cap, ctx);
 
         // Set the "actual" size of the canvas
         canvas.width = rect.width * dpr;
@@ -50,22 +46,61 @@
                     )
                 );
                 points.push(group[i]);
-                qtree.insert(new Point(group[i].x, group[i].y, c));
+                // qtree.insert(new Point(group[i].x, group[i].y, c));
             }
             return group;
         };
+        const rule = (atoms1, atoms2, g) => {
+            for (let i = 0; i < atoms1.length; i++) {
+                let fx = 0;
+                let fy = 0;
+                let a = atoms1[i];
+                // if(i==0){
+                //     let fg = qtree.query(new Circle(a.x,a.y,local))
+                //     console.log(fg)
+                // }
 
+                for (let j = 0; j < atoms2.length; j++) {
+                    let b = atoms2[j];
+                    let dx = a.x - b.x;
+                    let dy = a.y - b.y;
+                    let d = Math.sqrt(dx * dx + dy * dy);
+                    if (d > 0 && d < local) {
+                        let F = (g * 1) / d;
+                        fx += F * dx;
+                        fy += F * dy;
+                    }
+                }
+                a.vx = (a.vx + fx * damp) * 0.5;
+                a.vy = (a.vy + fy * damp) * 0.5;
+
+                if (a.x <= 0 || a.x >= w) {
+                    a.vx *= -2;
+                }
+                if (a.y <= 0 || a.y >= h) {
+                    a.vy *= -2;
+                }
+
+                a.x += a.vx;
+                a.y += a.vy;
+                if (a.x < margin) {a.vx = 1;a.x=margin}
+                if (a.x > w - margin) {a.vx = -1;a.x=w-margin}
+                if (a.y < margin) {a.vy = 1;a.y=margin}
+                if (a.y > h- margin) {a.vy = 1;a.y=margin}
+            }
+        };
         canvas.style.opacity = 1;
 
         const draw = (x, y, c, s) => {
             ctx.fillStyle = c;
             ctx.fillRect(x - s / 2, y - s / 2, s, s);
         };
-
-        const size = Math.min(w, h);
-        let white = create(size * 1.5, "white", 1.5);
-        let red = create(size * 1.25, "red", 2);
-        let blue = create(size, "cyan", 3);
+        // let yellow = create(300, "yellow");
+        const S = Math.min(Math.min(h,w),800)
+        local = S / 6
+        let white = create((S * 2) , "white", 1);
+        let red = create((S * 1.5), "red", 2);
+        let blue = create(S, "cyan", 3);
         // let white = create(3, "white", 1.5);
         // let red = create(3, "red", 2);
         // let blue = create(100, "cyan", 3);
@@ -74,76 +109,35 @@
             points.push(point(m.x, m.y, "green", 2));
             qtree.insert(new Point(m.x, m.y, "green"));
         });
-        let rules = {};
-        const evalF = (p, rules, i) => {
-            let fx = 0;
-            let fy = 0;
-            let fg = qtree.query(new Circle(p.x, p.y, local));
-            if (i == 0) draw(p.x, p.y, "purple", 10);
-            for (let ix in fg) {
-                const f = fg[ix];
-                if (f.x == p.x && f.y == p.y) continue;
-                let dx = p.x - f.x;
-                let dy = p.y - f.y;
-                let d = Math.sqrt(dx * dx + dy * dy);
-                let g = rules[p.c]?.[f.c] ?? 0;
-                if(d>0){
-                    let F = (g * 1) / d;
-                fx += F * dx;
-                fy += F * dy;
-                }
-                if (i == 0) draw(f.x, f.y, "green", 10);
-            }
-            p.vx = (p.vx + fx * damp) * 0.5;
-            p.vy = (p.vy + fy * damp) * 0.5;
-            p.x += p.vx;
-            p.y += p.vy;
-            if (p.x < margin) {
-                p.vx = 2;
-                p.x = margin;
-            }
-            if (p.x > w - margin) {
-                p.vx = -2;
-                p.x = w - margin;
-            }
-            if (p.y < margin) {
-                p.vy = 2;
-                p.y = margin;
-            }
-            if (p.y > h - margin) {
-                p.vy = -2;
-                p.y = h - margin;
-            }
-        };
 
         function update() {
-            rules["white"] = {
-                white: 0.002,
-                red: -0.2 - 0.2 * (y / h),
-                cyan: 0.04 + 0.03 * (y / h),
-            };
-            rules["red"] = { 
-                white: -2 + 1.7 * (y / h), 
-                red: 0.01, 
-                cyan: -0.25 };
-            rules["cyan"] = {
-                white: -0.005,
-                red: -0.2 - 0.1 * (y / h),
-                cyan: 0.01 + 2 * Math.sin((3.14 * 2 * y) / h) + 0.2 * (y / h),
-            };
+            rule(red, red, 0.04 + 2 * Math.sin(((3.14 * 2 * y) / h) * 2));
+            rule(white, white, 0.02);
+            rule(white, blue, 0.04 + 0.03 * (y / h));
+            rule(blue, white, 0.005);
+            rule(blue, red, -0.2 - 0.1 * (y / h));
+            rule(red, blue, -0.5);
+            rule(
+                blue,
+                blue,
+                0.1 + 2 * Math.sin((3.14 * 2 * y) / h) - 0.2 * (y / h)
+            );
+            rule(white, red, -0.2 - 0.2 * (y / h));
+            rule(red, white, -2 + 0.7 * (y / h));
 
             ctx.fillStyle = "rgb(10,10,10)";
             ctx.fillRect(0, 0, w, h);
-            qtree = new QuadTree(boundary, 5, ctx);
+            // qtree = new QuadTree(boundary, 5, ctx);
 
             for (let i = 0; i < points.length; i++) {
-                qtree.insert(new Point(points[i].x, points[i].y, points[i].c));
+                const p = points[i];
+                draw(p.x, p.y, p.c, 2);
             }
-            for (let i = 0; i < points.length; i++) {
-                evalF(points[i], rules);
-            }
-            qtree.show();
+            // evalF(points[0],rules)
+            // qtree.show();
             ctx.save();
+            // console.log("X::vel",points[0].vx,"pos",points[0].x);
+            // console.log("Y::vel",points[0].vy,"pos",points[0].y);
 
             requestAnimationFrame(update);
         }
