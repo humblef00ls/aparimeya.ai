0403c1619441075ca46c1a5346a30466f050e2e7
Aparimeya Taneja <42758991+humblef00ls@users.noreply.github.com>
2022-12-28 07:23:11 +0000

    logging

--- src/lib/Simulation.svelte
+++ src/lib/Simulation.svelte
@@ -3,23 +3,27 @@
     import { onMount } from "svelte";
     let fps = 0;
     export let y = 0;
-    let S = 100;
+    let S = 700;
     let m = { x: 0, y: 0 };
-    let local = 100;
+    let local = 90;
     $: ll =
         Math.max(local, Math.min(w, h) / 8) +
         Math.abs(50 * Math.sin((3.14 * y) / h)) +
-        HS * 10;
+        HS * 15;
     let damp = 0.8;
     $: dd = damp - HS * 0.05;
     $: HS = y / h;
+    $: SIN = Math.sin(3.14*y/h)
     const margin = 5;
     let [h, w] = [0, 0];
-    function handleMousemove(event) {
-        m.x = event.clientX;
-        m.y = event.clientY;
-    }
-    let redD = {};
+    // function handleMousemove(event) {
+    //     m.x = event.clientX;
+    //     m.y = event.clientY;
+    // }
+    let RD = {};
+    let WD = {};
+    let BD = {};
+    let PD = {};
     onMount(() => {
         h = window.innerHeight;
         w = window.innerWidth;
@@ -86,26 +90,20 @@
                 a.vx = (a.vx / scaler + fx) * 0.5;
                 a.vy = (a.vy / scaler + fy) * 0.5;
 
-                // if (a.x <= 0 || a.x >= w) {
-                //     a.vx *= -2;
-                // }
-                // if (a.y <= 0 || a.y >= h) {
-                //     a.vy *= -2;
-                // }
 
                 a.x += a.vx;
                 a.y += a.vy;
                 if (a.x < margin) {
-                    a.vx = 1;
+                    a.vx = a.vx + 20;
                     a.x = margin;
                 } else if (a.x > w - margin) {
-                    a.vx = -1;
+                    a.vx = a.vx - 20;
                     a.x = w - margin;
                 } else if (a.y < margin) {
-                    a.vy = 1;
+                    a.vy = a.vy + 20;
                     a.y = margin;
                 } else if (a.y > h - margin) {
-                    a.vy = -1;
+                    a.vy = a.vy - 20;
                     a.y = h - margin;
                 }
             }
@@ -117,12 +115,12 @@
             ctx.fillRect(x - s / 2, y - s / 2, s, s);
         };
 
-        S = Math.min(Math.min(h, w), 800);
+        const scaleX = Math.min(Math.min(h, w), S);
 
-        let white = create(S * 2.2, "white", 1);
-        let red = create(S * 1.75, "red", 2);
-        let blue = create(S / 1.1, "DarkTurquoise", 3);
-        let purple = create(S / 4, "purple", 4);
+        let white = create(scaleX * 2.4, "white", 1);
+        let red = create(scaleX * 1.8, "red", 2);
+        let blue = create(scaleX / 1.1, "DarkTurquoise", 3);
+        let purple = create(scaleX / 5, "purple", 4);
         const times = [];
 
         function update() {
@@ -132,33 +130,47 @@
             }
             times.push(now);
             fps = times.length;
-            redD = {
-                red: 0.04 - 0.004 * HS + 2 * Math.sin((3.14 * 2 * y) / h),
-                blue: -1 + 0.23 * HS,
-                white: 0.2 - 0.09 * HS,
+            RD = {
+                r: 0.04 - 0.004 * HS + 2 * SIN,
+                b: -1 + 0.23 * HS,
+                w: 0.2 - 0.09 * HS,
+                p: 0.4 - 0.5 * HS,
             };
-            rule(red, red, redD.red);
-            rule(red, blue, redD.blue);
-            rule(red, white, redD.white);
+            WD = {
+                r: -0.2 - 0.12 * HS,
+                b: -0.04 + 0.03 * HS,
+                w: 0.2 - 0.04 * HS,
+                p: 0.02,
+            };
+            BD = {
+                r: -0.2 - 0.1 * HS,
+                b: 0.5 + 2 * SIN - 0.26 * HS,
+                w: -0.05,
+                p: -0.2,
+            };
+            PD = {
+                r:-1 + 0.15 * HS,
+                b:-0.3,
+                w:-0.01 - 0.02 * HS,
+                p:-0.1 + 0.08 * HS
+            }
+            rule(red, red, RD.r);
+            rule(red, blue, RD.b);
+            rule(red, white, RD.w);
+            rule(red, purple, RD.p);
+            rule(white, red, WD.r);
+            rule(white, blue, WD.b);
+            rule(white, white, WD.w);
+            rule(white, purple, WD.p);
+            rule(blue, white, BD.w);
+            rule(blue, red, BD.r);
+            rule(blue, blue, BD.b);
+            rule(blue, purple, BD.p);
 
-            rule(white, white, 0.2 - 0.04 * (HS));
-            rule(white, blue, -0.04 + 0.03 * (HS));
-            rule(blue, white, -0.05);
-            rule(blue, red, -0.2 - 0.1 * HS);
-
-            rule(
-                blue,
-                blue,
-                0.5 + 2 * Math.sin((3.14 * 2 * y) / h) - 0.26 * HS
-            );
-            rule(white, red, -0.2 - 0.12 *HS);
-            rule(purple, white, -0.01 - 0.02 * HS);
-            rule(white, purple, 0.02);
-            rule(purple, purple, -0.1 + 0.08 * HS);
-            rule(purple, red, -1 + 0.15 *HS);
-            rule(purple, blue, -0.3);
-            rule(blue, purple, -0.2);
-            rule(red, purple, 0.4 - 0.5 * HS);
+            rule(purple, white, PD.w );
+            rule(purple, purple,PD.p );
+            rule(purple, red,PD.r );
+            rule(purple, blue, PD.b);
 
             ctx.fillStyle = "rgb(11,11,11)";
             ctx.fillRect(0, 0, w, h);
@@ -178,21 +190,41 @@
             cancelAnimationFrame(frameId);
         };
     });
+
+    const lim = (o) =>
+        typeof o === "object" && o !== null
+            ? Object.fromEntries(Object.entries(o).map(([k, v]) => [k, lim(v)]))
+            : Math.round(o * 100) / 100;
 </script>
 
 <div class="panel">
     F:{fps}
     R:
-    {ll}
+    {lim(ll)}
     S:
-    {S}
+    {lim(S)}
     D:
-    {dd}
+    {lim(dd)}
+    Sin:
+    {lim(SIN)}
     <div class="sub-panel">
-        reds : {JSON.stringify(redD)}
+        <span>
+        red : {JSON.stringify(lim(RD))}
+    </span>
+    <span>
+        white : {JSON.stringify(lim(WD))}
+    </span>
+    <span>
+        
+        blue : {JSON.stringify(lim(BD))}
+
+    </span>
+    <span>
+        purple : {JSON.stringify(lim(PD))}
+    </span>
     </div>
 </div>
-<svelte:window on:mousemove={handleMousemove} />
+<!-- <svelte:window on:mousemove={handleMousemove} /> -->
 <canvas bind:this={canvas} />
 
 <style>
@@ -213,11 +245,15 @@
         z-index: 7;
         opacity: 1;
         padding: 10px;
-        backdrop-filter: blur(3px);
         border-radius: 5px;
     }
     .sub-panel {
         padding: 5px;
         background-color: rgba(255, 255, 255, 0.05);
+        display:flex;
+        flex-direction: column;
+    }
+    .sub-panel > span{
+
     }
 </style>
