661788a95a1c2fb4355b40e3fa4542d6e34b660d
Aparimeya Taneja <42758991+humblef00ls@users.noreply.github.com>
2023-01-02 19:51:06 +0530

    Update projects.js

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -8,7 +8,12 @@ export default [
         title:"aparimeya.[_]",
         skills:"FE",
         img:"portfolio",
-        desc:`This webiste  <br>  Yes? first <a href='https://aparimeya.ai'> link </a> & second <a href='https://aparimeya.xyz'> likns</a>`,
+        desc:`As an engineer with a diverse skillset, from coding focused disciplines like full stack dev to creative pursuits like digital art to now new frontiers like prompt engineering, I wanted to remake my portfolio website to reflect my unique profile. \ 
+        In 2022, after I started using generative AI [GPT, SD, etc.] to make business solutions, art, etc., I decided this is the area I will bet my future on, and thus wanted to incorporate it as a core theme in  <a href='https://aparimeya.ai'>this website.</a> \
+        But of course, what does a particle simulation have to do with AI? Well, when I think of "intellegence", what comes to mind isn't nueral networks but rather something even more fundamental: emergence. \
+        It's all around us and yet we rarely think about it in its most basic form, and so I figured this would make for an interesting and interactive centerpiece. The order and patterns you observe in the chaotic soup of pixels flying around is not defined anywhere in the code; this is all just a result of a few basic rules that define how the 4 different \
+        types of particles attract and repel each other [these values change as you scroll thus leading to new kinds of patterns]. 
+        <br> Feel free to play around with the simulation hyper paramters [by clicking the cog icon in the bottom left] & check out my  <a href='https://aparimeya.xyz'>previous portfolio website</a> that I left up because I really like the centerpiece in that one too (space themed since I think space is really cool!)`,
     },
     {
         title:"humblef00ls",
